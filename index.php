955e18f79a92c1ec8adf0693e4c1d0cb771b98cc
Desiree <desireesanchezdev@gmail.com>
2024-01-21 22:22:48 +0100

    Reestructuring files

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $messages = processGuess();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Juego del Ahorcado</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
